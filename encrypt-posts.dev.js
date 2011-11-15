4cc94f2a22d3290fcfb21221add00b3a9fd33df3
Benjamin J. Balter <ben@balter.com>
2011-11-15 15:17:49 -0500

    comments

--- encrypt-posts.dev.js
+++ encrypt-posts.dev.js
@@ -1,14 +1,17 @@
 jQuery( document ).ready( function( $ ){
 	
+	//decrypt prompt
 	if ( pagenow == 'post' && encrypt_posts.prompt == 1 ) {
 		tb_show( 'Password', '?TB_inline=true&inlineId=ep_password_prompt&modal=true' );
 	}
 	
+	//password submit
 	$("#ep_password_submit").click( function() {
 		$("form#ep_password_form").submit();
 		return false;
 	});
 	
+	//require password prior to submitting
 	$("#publish").click( function( e ) {
 	
 		if ( $('#ep_toggle').is(':checked') && $('#ep_password').val() == '' ) {
--- encrypt-posts.php
+++ encrypt-posts.php
@@ -16,9 +16,25 @@ class Encrypt_Posts {
 	private $encrypts = array();
 	
 	function __construct() {
+
+		//load cryptography class and init			
+		$this->init_cryptography();
+		
+		add_filter( 'content_save_pre', array( &$this, 'pre_save_filter' ), 10, 1 );
+		add_filter( 'content_edit_pre', array( &$this, 'pre_content_edit_filter' ), 10, 1 );
+		add_action( 'add_meta_boxes', array( &$this, 'add_meta_box' ) );
+		add_action( 'admin_head', array( &$this, 'enqueue_js' ) );
+		add_action( 'admin_footer', array( &$this, 'password_prompt' ) );
+		add_filter( 'the_content', array( &$this, 'content_filter' ) );
+	}
+	
+	/**
+	 * Loads cryptography class and init's; sets up salt
+	 */
+	function init_cryptography() {
 		
 		$crypt_path = dirname( __file__ ) . '/' . 'class.cryptastic.php';
-		
+
 		if ( !file_exists( $crypt_path ) )
 			die( 'Can\'t load Cryptasic class. Tried ' . $crypt_path );
 			
@@ -30,15 +46,14 @@ class Encrypt_Posts {
 			die( 'Need salts added to wp-config' );
 			
 		$this->salt = SECURE_AUTH_SALT;	
-		
-		add_filter( 'content_save_pre', array( &$this, 'pre_save_filter' ), 10, 1 );
-		add_filter( 'content_edit_pre', array( &$this, 'pre_content_edit_filter' ), 10, 1 );
-		add_action( 'add_meta_boxes', array( &$this, 'add_meta_box' ) );
-		add_action( 'admin_head', array( &$this, 'enqueue_js' ) );
-		add_action( 'admin_footer', array( &$this, 'password_prompt' ) );
-		add_filter( 'the_content', array( &$this, 'content_filter' ) );
 	}
 	
+	/**
+	 * Encrypt a string
+	 * @param string $data the string to encrypt
+	 * @param string $pass the password
+	 * @return string the encrypted string
+	 */
 	function encrypt( $data, $pass = null ) {
 	
 		//note: base64 encoded to prevent encoding problems in the DB
@@ -47,6 +62,12 @@ class Encrypt_Posts {
 	
 	}
 	
+	/**
+	 * Decrypt a string
+	 * @param string $data encrypted string
+	 * @param string $pass the password
+	 * @return string|bool unencrypted string or false on failure
+	 */
 	function decrypt( $data, $pass = null ) {
 	
 		$key = $this->get_key( $pass );
@@ -54,6 +75,11 @@ class Encrypt_Posts {
 	
 	}
 	
+	/** 
+	 * Builds a key from a password
+	 * @param string $pass the password
+	 * @return string the key
+	 */
 	function get_key( $pass = null ) {
 		
 		if ( $pass == null && isset( $_POST['ep_password'] ) )
@@ -63,15 +89,22 @@ class Encrypt_Posts {
 	
 	}
 	
+	/**
+	 * Encrypts post prior to hitting database
+	 * @param string $content the raw content
+	 * @return string the encrypted content
+	 */
 	function pre_save_filter( $content ) {
 
 		global $post;
 	
+		//verify user wants encryption, otherwise remove flag
 		if ( !isset( $_POST['ep_toggle'] ) || !$_POST['ep_toggle'] ) {
 			delete_post_meta( $post->ID, '_encrypt_post' );
 			return $content;
 		}
-					
+		
+		//make sure we have a password
 		if ( $_POST['ep_toggle'] && empty( $_POST['ep_password'] ) )
 			die( 'Cannot encrypt without password' );
 		
@@ -84,17 +117,26 @@ class Encrypt_Posts {
 		if ( !$content )
 			die( 'Encryption error' );
 			
+		//set flags
 		add_post_meta( $post->ID, '_encrypt_post', true, true );
 		$this->encrypts[] = $post->ID;
 		
 		return $content;
 	}
 	
+	/**
+	 * Register metaboxes
+	 */
 	function add_meta_box( ) {
 		add_meta_box( 'encrypt_posts', 'Encryption', array( &$this, 'metabox' ), 'post' );	
 		add_meta_box( 'encrypt_posts', 'Encryption', array( &$this, 'metabox' ), 'page' ); 	
 	}
 	
+	/**
+	 * Checks whether a given post is encrypted
+	 * @param int $postID the post to check
+	 * @return bool true if encrypted
+	 */
 	function encrypted_post( $postID = null) {
 		global $post;
 		
@@ -104,6 +146,9 @@ class Encrypt_Posts {
 		return get_post_meta( $postID, '_encrypt_post', true );
 	}
 	
+	/**
+	 * Callback to generate encryption metabox
+	 */
 	function metabox( $post ) {
 	?>
 	<p>
@@ -114,6 +159,9 @@ class Encrypt_Posts {
 	<?php 
 	}
 	
+	/**
+	 * Callback to generate password prompt
+	 */
 	function password_prompt() { 
 		global $pagenow;
 		global $post;
@@ -138,6 +186,9 @@ class Encrypt_Posts {
 	<?php
 	}
 	
+	/**
+	 * Decrypt post prior to editing
+	 */
 	function pre_content_edit_filter( $content ) {
 	
 		global $post;
@@ -157,6 +208,9 @@ class Encrypt_Posts {
 		
 	}
 	
+	/**
+	 * Register JavaScript file
+	 */
 	function enqueue_js() {
 	
 		$suffix = ( WP_DEBUG ) ? 'dev.' : '';
@@ -173,6 +227,9 @@ class Encrypt_Posts {
 		wp_localize_script( 'encrypt_posts', 'encrypt_posts', $data ); 
 	}
 	
+	/**
+	 * front-end content filter
+	 */
 	function content_filter( $content ) {
 		
 		if ( !$this->encrypted_post( $post->ID ) )
