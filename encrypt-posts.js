a9f9d593a49bab69e7c6d21663bcfa206457be5d
Benjamin J. Balter <ben@balter.com>
2011-11-15 15:09:08 -0500

    minified js

---
+++ encrypt-posts.js
@@ -0,0 +1,2 @@
+jQuery(document).ready(function(a){pagenow=="post"&&encrypt_posts.prompt==1&&tb_show("Password","?TB_inline=true&inlineId=ep_password_prompt&modal=true");a("#ep_password_submit").click(function(){a("form#ep_password_form").submit();return false});a("#publish").click(function(b){if(a("#ep_toggle").is(":checked")&&a("#ep_password").val()=="")return b.preventDefault(),b.stopPropagation(),alert(encrypt_posts.noPassWarning),a("#ajax-loading").hide(),setTimeout("jQuery('#publish').removeClass('button-primary-disabled')",
+1),false});autosave=function(){}});
